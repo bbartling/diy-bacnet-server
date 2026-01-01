1039e501d0ac708d66920c1c51b83cdbbae6a50b
Ben Bartling <ben.bartling@gmail.com>
2026-01-01 09:53:09 -0600

    changed chars on fast api swagger

--- bacpypes_server/rpc_app.py
+++ bacpypes_server/rpc_app.py
@@ -3,7 +3,7 @@ import fastapi_jsonrpc as jsonrpc
 from fastapi.responses import RedirectResponse
 from bacpypes_server.rpc_methods import rpc
 
-rpc_api = jsonrpc.API(title="DIY BACnet Server", version="1.0", description="The BACnet RPC Gateway for the DIY Agent Manager")
+rpc_api = jsonrpc.API(title="diy-bacnet-server", version="1.0", description="The BACnet RPC Gateway for the DIY Agent Manager")
 rpc_api.bind_entrypoint(rpc)
 
 
